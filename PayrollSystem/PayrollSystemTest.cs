34039a9042a0663f2ce8154397b2a13e4f4d5c46
kelec <kelec@LAPTOP-JGFOV90A>
2023-04-02 17:15:01 -0400

    Add project files.

---
+++ PayrollSystem/PayrollSystemTest.cs
@@ -0,0 +1,65 @@
+﻿// GRADING ID HERE: Kelechi Ragland 5379658
+
+using System;
+using System.Collections.Generic;
+using static System.Console;
+
+// 1. Create two TipsPlusHourlyEmployee objects (under base-salaried commission employee
+//    Bob Lewis, right under existing lines 21-23).
+// 2. Add your two new employees to the the list (in existing lines 26-27).
+class PayrollSystemTest
+{
+   static void Main()
+   {
+      // create derived-class objects
+      var salariedEmployee = new SalariedEmployee("John", "Smith",
+          "111-11-1111", 800.00M);
+      var hourlyEmployee = new HourlyEmployee("Karen", "Price",
+         "222-22-2222", 16.75M, 40.0M);
+      var commissionEmployee = new CommissionEmployee("Sue", "Jones",
+         "333-33-3333", 10000.00M, .06M);
+      var basePlusCommissionEmployee =
+         new BasePlusCommissionEmployee("Bob", "Lewis",
+         "444-44-4444", 5000.00M, .04M, 300.00M);
+      var tipsPlusHourlyEmployee =
+         new TipsPlusHourlyEmployee("Steven", "Daniel",
+         "444-44-4443", 6000.00M, .03M, 900.00M, 70.00M);
+         new TipsPlusHourlyEmployee("Chris", "Tucker",
+         "444-44-4455", 3000.00M, .04M, 200.00M, 50.00M);
+
+        // create List<Employee> and initialize with employee objects
+        var employees = new List<Employee>() {salariedEmployee,
+         hourlyEmployee, commissionEmployee, basePlusCommissionEmployee};
+
+      WriteLine("Employees processed polymorphically:\n");
+
+      // generically process each element in employees
+      foreach (var currentEmployee in employees)
+      {
+         WriteLine(currentEmployee); // invokes ToString
+         WriteLine($"earned: {currentEmployee.Earnings():C}\n");
+      }
+
+      // get type name of each object in employees 
+      for (int j = 0; j < employees.Count; j++)
+      {
+         WriteLine($"Employee {j} is a {employees[j].GetType()}");
+      }
+   }
+}
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2017 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
---
+++ PayrollSystem/TipsPlusHourlyEmployee.cs
@@ -0,0 +1,87 @@
+﻿// GRADING ID HERE: Kelechi Ragland 5379658
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Make the following changes/additions:
+// 1. TipsPlusHourlyEmployee IS-A HourlyEmployee
+// 2. Add public decimal property named TotalTips with get and set accessors
+//    for the new field. Be sure to validate the value being set to ensure
+//    that it is non-negative. Otherwise, throw an ArgumentOutOfRangeException.
+// 3. Add public decimal property named TipTake with get and set accessors for
+//    the new field. Be sure to validate the value being set to ensure that
+//    it is between 0-1 (inclusive). Otherwise, throw an ArgumentOutOfRangeException.
+// 4. Add a constructor that initializes the TipsPlusHourlyEmployee with the
+//    given values for first name, last name, social security number,
+//    hourly wage, hours worked, total tips, and tip take percentage.
+// 5. Override method Earnings so that it calculates the employee’s earnings
+//    by adding their tip take percentage of the total tips earned (the product
+//    of their tip take and total tips) onto their earnings as an hourly employee.
+//    You must use base.Earnings() in your implementation.
+// 6. Override the ToString method to return a String that will display all
+//    data fields neatly. You must use base.ToString() in your implementation.
+public class TipsPlusHourlyEmployee : HourlyEmployee
+{
+    private decimal tipTotal;
+    private decimal tipTale;
+
+    public TipsPlusHourlyEmployee(string firstName, string lastName,
+      string socialSecurityNumber, decimal hourlyWage,
+      decimal hoursWorked, decimal totalTips, decimal tipTake) : base(firstName, lastName,
+       socialSecurityNumber, hourlyWage,
+      hoursWorked)
+    {
+        TotalTips = totalTips;
+        TipTake = tipTake;
+    }
+
+    public decimal TotalTips
+    {
+        get { return tipTotal; }
+        set
+        {
+            if (value >= 0)
+            {
+                tipTotal = value;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(TotalTips)}", value, $"{nameof(TotalTips)} must be >= 0");
+            }
+        }
+    }
+
+    public decimal TipTake
+    {
+        get { return tipTale; }
+        set
+        {
+            if (value >= 0 && value <= 1)
+            {
+                tipTale = value;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(TipTake)}", value, $"{nameof(TipTake)} must be >= 0");
+            }
+        }
+    }
+
+    public override decimal Earnings()
+    {
+        return (TipTake * TotalTips) + base.Earnings();
+    }
+
+    public override string ToString()
+    {
+        string NL = Environment.NewLine;
+
+        return $"Tips{base.ToString()}{NL}Total Tips: {TotalTips}{NL}Tip Take: {TipTake}";
+    }
+
+
+}
+
